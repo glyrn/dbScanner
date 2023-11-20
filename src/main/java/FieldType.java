3bef1ce1f73e163e199a6d7cc6ff0642b5565f8c
gly <reen.gly@qq.com>
2023-11-21 01:49:43 +0800

    feat: 字段枚举类型

--- src/main/java/FieldType.java
+++ src/main/java/FieldType.java
@@ -1,24 +1,50 @@
 /**
- * 字段类型
+ * 字段枚举类型
  *
- * 数字类型 byte short int long float double
- *
- * 二进制 blob medium blob
- *
- * 字符串类型 varchar text mediumtext long text
- *
- * 未指定 null
- *
- * 默认字符集
+ * @author gly
  */
 public enum FieldType {
-/**
- * 使用 class<?>[] 记录变量类型
- *
- * String 记录字段类型
- *
- * 长度
- *
- * 字符集
- */
+
+    BYTE(new Class[]{Byte.class}, "byte", 0, "defaultCharset"),
+    SHORT(new Class[]{Short.class}, "short", 0, "defaultCharset"),
+    INT(new Class[]{Integer.class}, "int", 0, "defaultCharset"),
+    LONG(new Class[]{Long.class}, "long", 0, "defaultCharset"),
+    FLOAT(new Class[]{Float.class}, "float", 0, "defaultCharset"),
+    DOUBLE(new Class[]{Double.class}, "double", 0, "defaultCharset"),
+
+    BLOB(new Class[]{Byte[].class}, "blob", 0, "defaultCharset"),
+    MEDIUM_BLOB(new Class[]{Byte[].class}, "medium blob", 0, "defaultCharset"),
+
+    VARCHAR(new Class[]{String.class}, "varchar", 255, "defaultCharset"),
+    TEXT(new Class[]{String.class}, "text", 0, "defaultCharset"),
+    MEDIUM_TEXT(new Class[]{String.class}, "mediumtext", 0, "defaultCharset"),
+    LONG_TEXT(new Class[]{String.class}, "longtext", 0, "defaultCharset"),
+
+    NULL(null, "null", 0, "defaultCharset");
+
+
+    /**
+     * 变量类型
+     */
+    private Class<?>[] fieldTypeClass;
+    /**
+     * 字段类型
+     */
+    private String fieldType;
+    /**
+     * 字段长度
+     */
+    private int length;
+    /**
+     * 字符集
+     */
+    private String charSet;
+
+    FieldType(Class<?>[] fieldTypeClass, String fieldType, int length, String charSet) {
+        this.fieldTypeClass = fieldTypeClass;
+        this.fieldType = fieldType;
+        this.length = length;
+        this.charSet = charSet;
+    }
+
 }
