f9e0b4656fe17af45892dceea2d1a0203c27098b
gly <reen.gly@qq.com>
2024-01-31 21:44:35 +0800

    feat: 1. 扫表前置准备 2. md5实体类

--- src/main/java/DBScanner.java
+++ src/main/java/DBScanner.java
@@ -1,9 +1,17 @@
+import bean.DBScannerEntityMD5;
+import bean.TableName;
+import com.google.protobuf.Internal;
 import conf.DbCfg;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
@@ -95,10 +103,89 @@ public class DBScanner {
      */
     private boolean scanningDatabase(DBExcutor dbExcutor, String tablePackages) {
         try{
+            List<String> tableNames = dbExcutor.getAllTablesName();
+
+            Set<String> packageNames = new LinkedHashSet<>();
+
+            // 创建md5表
+            createTableMd5(dbExcutor, tableNames);
+
+            // 遍历所有需要自动扫库的entity
+            Map<String, List<Class<?>>> entities = new HashMap<>();
+            Map<String, List<Class<?>>> batchConstrains = new HashMap<>();
+
+            String[] entityPackageArray = tablePackages.split(",");
+
+            if (entityPackageArray != null) {
+                for (String entityPackage : entityPackageArray) {
+                    entityPackage = entityPackage.trim();
+                    {// entity
+                        // TODO 扫描所有带 @Entity 注解的class [静态] [classSet]
+                        List<Class<?>> classSet = new ArrayList<>();
+                        Iterator<Class<?>> ite = classSet.iterator();
+
+                        while (ite.hasNext()) {
+                            Class<?> objClass = ite.next();
+                            // 过滤没有 @TableName 注解的类
+                            if (!objClass.isAnnotationPresent(TableName.class)) {
+                                ite.remove();
+                            }
+                        }
+                        if (classSet.size() > 0) {
+                            packageNames.add(entityPackage);
+                            entities.put(entityPackage, classSet);
+                        }
+                    }
+                    {// batchConstrants
+                        //TODO: 向前兼容层
+                    }
+                }
+
+            }
+
+            // 旧的md5
+            if (oldFileMd5Map.size() <= 0) {
+
+                List<Map<String, Object>> list = dbExcutor.qurey("select packageName,className,md5,tableName from " + TABLE_MD5);
+                if (list != null && list.size() > 0) {
+                    for (Map<String, Object> objectMap : list) {
+                        DBScannerEntityMD5 entityMD5 = new DBScannerEntityMD5();
+
+                        entityMD5.setPackageName(String.valueOf(objectMap.get("packageName")));
+                        entityMD5.setClassName(String.valueOf(objectMap.get("className")));
+                        entityMD5.setMd5(String.valueOf(objectMap.get("md5")));
+                        entityMD5.setTableName(String.valueOf(objectMap.get("tableName")));
+
+                        if (!packageNames.contains(entityMD5.getPackageName())) {
+                            // 数据库中容易的包不再处理
+                            tableNames.remove(entityMD5.getTableName());
+                            continue;
+                        }
+
+                        oldFileMd5Map.put(entityMD5.getClassName(), entityMD5.getMd5());
+                        noUseTableMap.put(entityMD5.getTableName(), entityMD5.getClassName());
+                    }
+                }
+
+            }
+            // 开始扫描
+
 
         }catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+        return true;
+    }
+
+    /**
+     *
+     * @param dbExcutor
+     * @param entities
+     * @param batchConstraints
+     * @param existTableNames
+     * @return
+     */
+    private boolean doScanningDatabase(DBExcutor dbExcutor, Map<String, List<Class<?>>> entities, Map<String, List<Class<?>>> batchConstraints, List<String> existTableNames) {
         return true;
     }
 
@@ -115,4 +202,29 @@ public class DBScanner {
         }
         return true;
     }
+
+
+
+    /**
+     * 创建classMd5表
+     */
+    private void createTableMd5(DBExcutor dbExcutor, List<String> tableNames) {
+        try{
+            Set<String> tableNameSet = new HashSet<>(tableNames);
+
+            // 如果是第一次使用 创建md5专用表 用于记录各表class的md5值
+
+            if (!tableNameSet.contains(TABLE_MD5)) {
+                dbExcutor.update("CREATE TABLE IF NOT EXISTS" + TABLE_MD5 + "(className varchar(120) not null primary key,packageName varchar(512),md5 varchar(100),tableName varchar(100) not null);" , false);
+            }
+
+        }catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static List<Class<?>> scanClassesFilter(String packageName, Class<?>... annotationClass) {
+        return null;
+    }
+
 }
--- src/main/java/bean/DBScannerEntityMD5.java
+++ src/main/java/bean/DBScannerEntityMD5.java
@@ -1,8 +1,47 @@
 package bean;
 
+import java.io.Serializable;
+
 /**
  * 专门存每一个表对应的md5值的表
  * data:
  */
-public class DBScannerEntityMD5 {
+public class DBScannerEntityMD5 implements Serializable {
+
+    private String packageName;
+    private String className;
+    private String md5;
+    private String tableName;
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
 }
